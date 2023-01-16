579eefb5a8b1d86e55d79b4e0e95c82a20c46764
Sprof22 <richmondelaigwu@gmail.com>
2023-01-16 14:52:21 +0100

    minor changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="city">Jos</div>
+      <div className="temperature">27</div>
+      <div className="weather-conditions">Mostly Cloudy</div>
+      <div className="temp-sum">
+        <div className="high">H:27</div>
+        <div className="low">L: 12</div>
+      </div>
     </div>
   );
 }
