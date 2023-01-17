87d9354c7430f90f57ba8b04ef30424c55ee2ed6
Sprof22 <richmondelaigwu@gmail.com>
2023-01-17 23:55:10 +0100

    forecast data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Search from "./components/search/Search";
 import CurrentWeather from "./components/current-weather/CurrentWeather";
 import { OPEN_WEATHER_API_KEY, OPEN_WEATHER_API_URL } from "./api";
 import { useState } from "react";
+import Forecast from "./components/forecast/Forecast";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
@@ -35,6 +36,7 @@ function App() {
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
--- src/components/forecast/Forecast.js
+++ src/components/forecast/Forecast.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
 
-const Forecast = () => {
+const Forecast = ({ data }) => {
   return (
-    <div>forecast</div>
-  )
-}
+    <>
+      <label htmlFor="" className="title">Daily</label>
+      <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((item, idx) => {
+          <AccordionItem key={idx}>
+            <AccordionItemHeading>
+                <AccordionItemButton>
+                hi
+                </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>;
+        })}
+      </Accordion>
+    </>
+  );
+};
 
-export default Forecast
+export default Forecast;
