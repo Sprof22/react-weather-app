e3cbbea1c92cdc2ee7c7d0b323169eab7e4c5a91
Sprof22 <richmondelaigwu@gmail.com>
2023-01-17 16:36:48 +0100

    format styling

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '402f188633mshaf1e63ec4ef6763p1e3620jsn52406261a821',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
